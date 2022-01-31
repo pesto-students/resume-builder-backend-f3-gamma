97c3cb8807dd7b939b64c4f4d8a711c1c377b6e7
ramshad ken <ramshadken2019@gmail.com>
2022-01-31 19:08:28 +0530

    add cross origin whitelist for frontend

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require("cors")
+
 
 var app = express();
+
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
